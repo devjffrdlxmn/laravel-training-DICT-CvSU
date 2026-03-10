b33f80d82849330df71b3b4eb3f4134d20ae42e6
devjffrdlxmn <108109722+devjffrdlxmn@users.noreply.github.com>
2026-03-10 08:25:43 +0800

    Day 1 finish

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Classes\ChallengeGenerator;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::name("game.")->group(function (){
+    Route::get('/game', function (Request $request, ChallengeGenerator $challengeGenerator){
+        if($request->session()->exists('game')){
+            $game = $request->session()->get('game');
+        }else{
+            $game = $challengeGenerator->generate();
+            $request->session()->put('game', $game);
+        }
+
+        if($game->isOver()){
+            $request->session()->forget('game');
+        }
+
+        $disabledKeys = $game->isOver() ? true : $game->getGuesses();
+        
+        return view('game.show', compact('game', 'disabledKeys'));
+    })->name('show');
+
+    Route::put('/game', function (Request $request) {
+        $game = $request->session()->get('game');
+
+        $skip = $request->input('skip', false);
+        if($skip){
+            $game->skip();
+        }else{
+            $guess = $request->input('guess');
+            $game->guess($guess);
+        }
+
+        return redirect()->route('game.show');
+    })->name('update');
+});
